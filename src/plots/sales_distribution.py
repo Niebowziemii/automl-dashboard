ff9af1f1256fa00fe3a9ad68c94a569501bf82d5
Franciszek Olejnik <18384894+FrankOil@users.noreply.github.com>
2023-11-29 22:16:52 +0100

    ✨ (src/plots/sales_distribution) Add filters to the plot

--- src/plots/sales_distribution.py
+++ src/plots/sales_distribution.py
@@ -19,14 +19,80 @@ def plot(data: dict[str, DataFrame], module: DeltaGenerator) -> None:
         data (dict[str, DataFrame]): M5 forecasting accuracy dict formatted as in load.py.
         module (DeltaGenerator): Layout element for rendering.
     """
-    n = 100
-    stv_ = data["stv"].sample(n=n, random_state=42)
+    n = 1000
 
-    stv_random = helper_func(stv_)
+    available_states = list(data["stv"]["state_id"].unique())
+    selected_states = module.multiselect(
+        "Select state:",
+        available_states,
+        available_states,
+    )
 
-    merged = stv_random.merge(data["calendar"], how="left", left_on="dates", right_on="d")
+    available_stores = list(
+        filter(
+            lambda store: store.startswith(tuple(selected_states)),
+            list(data["stv"]["store_id"].unique()),
+        )
+    )
+    selected_stores = module.multiselect(
+        "Select store:", available_stores, available_stores
+    )
+
+    available_categories = list(data["stv"]["cat_id"].unique())
+    selected_categories = module.multiselect(
+        "Select category:",
+        available_categories,
+        available_categories,
+    )
+
+    filtered_stv = data["stv"].query(
+        f"state_id in {selected_states} & store_id in {selected_stores} & cat_id in {selected_categories}"
+    )
+
+    if len(filtered_stv) == 0:
+        module.warning("No data available.")
+        return None
+
+    if len(filtered_stv) < n:
+        sample_stv = filtered_stv
+    else:
+        sample_stv = filtered_stv.sample(n=n, random_state=42)
+
+    merged = helper_func(sample_stv).merge(
+        data["calendar"], how="left", left_on="dates", right_on="d"
+    )
 
     merged_ = DataFrame(merged.groupby("date")["sales"].sum()).reset_index()
 
-    fig = px.line(merged_, x="date", y="sales", title=f"Sales Distribution of random {n} Samples", color_discrete_map={"color": "red"})
+    fig = px.line(
+        merged_,
+        x="date",
+        y="sales",
+        title=f"Sales over time of random {n} products",
+        labels={"date": "Date", "sales": "Sales"},
+        render_mode="webg1",
+    )
+
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list(
+                [
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=3, label="3m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all"),
+                ]
+            )
+        ),
+    )
+
+    fig.update_layout(
+        template="plotly_dark",
+        xaxis_rangeselector_font_color="white",
+        xaxis_rangeselector_activecolor="red",
+        xaxis_rangeselector_bgcolor="black",
+    )
+
     module.plotly_chart(fig)
